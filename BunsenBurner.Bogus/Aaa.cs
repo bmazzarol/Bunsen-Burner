016fc73ddba5f284216625e6c9f3113798c9469e
Ben Mazzarol <bmazzarol@gmail.com>
2022-09-27 15:26:01 +1000

    feat: Bogus support [ci]

---
+++ BunsenBurner.Bogus/Aaa.cs
@@ -0,0 +1,113 @@
+﻿using Bogus;
+
+namespace BunsenBurner.Bogus;
+
+using AaaScenario = Scenario<Syntax.Aaa>;
+
+/// <summary>
+/// Provides support for bogus in constructing arrange steps
+/// </summary>
+public static class Aaa
+{
+    /// <summary>
+    /// Auto arrange the scenario using bogus
+    /// </summary>
+    /// <param name="fn">async arrange function</param>
+    /// <typeparam name="TData">auto arranged data to act on</typeparam>
+    /// <returns>arranged scenario</returns>
+    [Pure]
+    public static AaaScenario.Arranged<TData> AutoArrange<TData>(Func<Faker<TData>, Task<TData>> fn)
+        where TData : class => Shared.AutoArrange<TData, Syntax.Aaa>(fn);
+
+    /// <summary>
+    /// Auto arrange the scenario using bogus
+    /// </summary>
+    /// <param name="fn">arrange function</param>
+    /// <typeparam name="TData">auto arranged data to act on</typeparam>
+    /// <returns>arranged scenario</returns>
+    [Pure]
+    public static AaaScenario.Arranged<TData> AutoArrange<TData>(Func<Faker<TData>, TData> fn)
+        where TData : class => Shared.AutoArrange<TData, Syntax.Aaa>(fn);
+
+    /// <summary>
+    /// Auto arrange the scenario using bogus
+    /// </summary>
+    /// <param name="name">name/description for the test</param>
+    /// <param name="fn">async arrange function</param>
+    /// <typeparam name="TData">auto arranged data to act on</typeparam>
+    /// <returns>arranged scenario</returns>
+    [Pure]
+    public static AaaScenario.Arranged<TData> AutoArrange<TData>(
+        this string name,
+        Func<Faker<TData>, Task<TData>> fn
+    ) where TData : class => name.AutoArrange<TData, Syntax.Aaa>(fn);
+
+    /// <summary>
+    /// Auto arrange the scenario using bogus
+    /// </summary>
+    /// <param name="name">name/description for the test</param>
+    /// <param name="fn">arrange function</param>
+    /// <typeparam name="TData">auto arranged data to act on</typeparam>
+    /// <returns>arranged scenario</returns>
+    [Pure]
+    public static AaaScenario.Arranged<TData> AutoArrange<TData>(
+        this string name,
+        Func<Faker<TData>, TData> fn
+    ) where TData : class => name.AutoArrange<TData, Syntax.Aaa>(fn);
+
+    /// <summary>
+    /// Auto arrange the scenario using bogus
+    /// </summary>
+    /// <param name="fn">async arrange function</param>
+    /// <param name="locale">locale</param>
+    /// <typeparam name="TData">auto arranged data to act on</typeparam>
+    /// <returns>arranged scenario</returns>
+    [Pure]
+    public static AaaScenario.Arranged<TData> AutoArrange<TData>(
+        Func<Faker, Task<TData>> fn,
+        string locale = "en"
+    ) => Shared.AutoArrange<TData, Syntax.Aaa>(fn, locale);
+
+    /// <summary>
+    /// Auto arrange the scenario using bogus
+    /// </summary>
+    /// <param name="fn">arrange function</param>
+    /// <param name="locale">locale</param>
+    /// <typeparam name="TData">auto arranged data to act on</typeparam>
+    /// <returns>arranged scenario</returns>
+    [Pure]
+    public static AaaScenario.Arranged<TData> AutoArrange<TData>(
+        Func<Faker, TData> fn,
+        string locale = "en"
+    ) => Shared.AutoArrange<TData, Syntax.Aaa>(fn, locale);
+
+    /// <summary>
+    /// Auto arrange the scenario using bogus
+    /// </summary>
+    /// <param name="name">name/description for the test</param>
+    /// <param name="fn">async arrange function</param>
+    /// <param name="locale">locale</param>
+    /// <typeparam name="TData">auto arranged data to act on</typeparam>
+    /// <returns>arranged scenario</returns>
+    [Pure]
+    public static AaaScenario.Arranged<TData> AutoArrange<TData>(
+        this string name,
+        Func<Faker, Task<TData>> fn,
+        string locale = "en"
+    ) => name.AutoArrange<TData, Syntax.Aaa>(fn, locale);
+
+    /// <summary>
+    /// Auto arrange the scenario using bogus
+    /// </summary>
+    /// <param name="name">name/description for the test</param>
+    /// <param name="fn">arrange function</param>
+    /// <param name="locale">locale</param>
+    /// <typeparam name="TData">auto arranged data to act on</typeparam>
+    /// <returns>arranged scenario</returns>
+    [Pure]
+    public static AaaScenario.Arranged<TData> AutoArrange<TData>(
+        this string name,
+        Func<Faker, TData> fn,
+        string locale = "en"
+    ) => name.AutoArrange<TData, Syntax.Aaa>(fn, locale);
+}
---
+++ BunsenBurner.Bogus/Shared.cs
@@ -0,0 +1,69 @@
+﻿using Bogus;
+
+namespace BunsenBurner.Bogus;
+
+/// <summary>
+/// Shared builder function implementations
+/// </summary>
+internal static class Shared
+{
+    [Pure]
+    internal static Scenario<TSyntax>.Arranged<TData> AutoArrange<TData, TSyntax>(
+        Func<Faker<TData>, Task<TData>> fn
+    )
+        where TData : class
+        where TSyntax : struct, Syntax => new(default, () => fn(new Faker<TData>()));
+
+    [Pure]
+    internal static Scenario<TSyntax>.Arranged<TData> AutoArrange<TData, TSyntax>(
+        Func<Faker<TData>, TData> fn
+    )
+        where TData : class
+        where TSyntax : struct, Syntax =>
+        AutoArrange<TData, TSyntax>(fixture => Task.FromResult(fn(fixture)));
+
+    [Pure]
+    internal static Scenario<TSyntax>.Arranged<TData> AutoArrange<TData, TSyntax>(
+        this string name,
+        Func<Faker<TData>, Task<TData>> fn
+    )
+        where TData : class
+        where TSyntax : struct, Syntax => new(name, () => fn(new Faker<TData>()));
+
+    [Pure]
+    internal static Scenario<TSyntax>.Arranged<TData> AutoArrange<TData, TSyntax>(
+        this string name,
+        Func<Faker<TData>, TData> fn
+    )
+        where TData : class
+        where TSyntax : struct, Syntax =>
+        name.AutoArrange<TData, TSyntax>(fixture => Task.FromResult(fn(fixture)));
+
+    [Pure]
+    internal static Scenario<TSyntax>.Arranged<TData> AutoArrange<TData, TSyntax>(
+        Func<Faker, Task<TData>> fn,
+        string? locale = "en"
+    ) where TSyntax : struct, Syntax => new(default, () => fn(new Faker(locale)));
+
+    [Pure]
+    internal static Scenario<TSyntax>.Arranged<TData> AutoArrange<TData, TSyntax>(
+        Func<Faker, TData> fn,
+        string? locale = "en"
+    ) where TSyntax : struct, Syntax =>
+        AutoArrange<TData, TSyntax>(fixture => Task.FromResult(fn(fixture)), locale);
+
+    [Pure]
+    internal static Scenario<TSyntax>.Arranged<TData> AutoArrange<TData, TSyntax>(
+        this string name,
+        Func<Faker, Task<TData>> fn,
+        string? locale = "en"
+    ) where TSyntax : struct, Syntax => new(name, () => fn(new Faker(locale)));
+
+    [Pure]
+    internal static Scenario<TSyntax>.Arranged<TData> AutoArrange<TData, TSyntax>(
+        this string name,
+        Func<Faker, TData> fn,
+        string? locale = "en"
+    ) where TSyntax : struct, Syntax =>
+        name.AutoArrange<TData, TSyntax>(fixture => Task.FromResult(fn(fixture)), locale);
+}
