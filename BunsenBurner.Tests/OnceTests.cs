3c7c82bebcf154062cb41874b99378415d4a44f3
Ben Mazzarol <bmazzarol@gmail.com>
2022-10-08 16:53:36 +0800

    feat: Once type for single execution shared values [ci]

---
+++ BunsenBurner.Tests/OnceTests.cs
@@ -0,0 +1,54 @@
+﻿using BunsenBurner.Utility;
+
+namespace BunsenBurner.Tests;
+
+using static Aaa;
+
+public static class OnceTests
+{
+    [Fact(DisplayName = "Once of T on ever runs once")]
+    public static async Task Case1() =>
+        await Arrange(Once.New(() => DateTime.Now))
+            .Act(async o =>
+            {
+                var d1 = await o;
+                var d2 = await o;
+                var d3 = await o;
+                return (d1, d2, d3);
+            })
+            .Assert(r => r.d1 == r.d2 && r.d1 == r.d3);
+
+    [Fact(DisplayName = "Once of T on ever runs once for an async function")]
+    public static async Task Case2() =>
+        await Arrange(
+                Once.New(async () =>
+                {
+                    await Task.Delay(TimeSpan.FromMilliseconds(10));
+                    return DateTime.Now;
+                })
+            )
+            .Act(async o =>
+            {
+                var d1 = await o;
+                var d2 = await o;
+                var d3 = await o;
+                return (d1, d2, d3);
+            })
+            .Assert(r => r.d1 == r.d2 && r.d1 == r.d3);
+
+    [Fact(DisplayName = "Once of T can be accessed in parallel without issue")]
+    public static async Task Case3() =>
+        await Arrange(
+                Once.New(async () =>
+                {
+                    await Task.Delay(TimeSpan.FromMilliseconds(10));
+                    return DateTime.Now;
+                })
+            )
+            .Act(async o =>
+            {
+                var result = Enumerable.Range(1, 1000).Select(async _ => await o);
+                return await Task.WhenAll(result);
+            })
+            .Assert(r => r.Distinct().Count() == 1);
+}
---
+++ BunsenBurner/Utility/Once.cs
@@ -0,0 +1,41 @@
+﻿namespace BunsenBurner.Utility;
+
+/// <summary>
+/// Simple async lazy that allows a test to pin data to a shared memory location and
+/// ensure it only executes once.
+/// </summary>
+/// <remarks>This is not for production code, its a simple type that has enough features for test scenarios</remarks>
+/// <typeparam name="T">some T</typeparam>
+public sealed class Once<T> : Lazy<Task<T>>
+{
+    internal Once(Func<T> valueFactory) : base(() => Task.Factory.StartNew(valueFactory)) { }
+
+    internal Once(Func<Task<T>> taskFactory)
+        : base(() => Task.Factory.StartNew(taskFactory).Unwrap()) { }
+
+    /// <summary>
+    /// Task awaiter for once
+    /// </summary>
+    /// <returns>task awaiter for T</returns>
+    public TaskAwaiter<T> GetAwaiter() => Value.GetAwaiter();
+}
+
+/// <summary>
+/// Static constructors for once
+/// </summary>
+public static class Once
+{
+    /// <summary>
+    /// Creates a new once
+    /// </summary>
+    /// <param name="fn">function</param>
+    /// <returns>once</returns>
+    public static Once<T> New<T>(Func<T> fn) => new(fn);
+
+    /// <summary>
+    /// Creates a new once
+    /// </summary>
+    /// <param name="fn">function</param>
+    /// <returns>once</returns>
+    public static Once<T> New<T>(Func<Task<T>> fn) => new(fn);
+}
