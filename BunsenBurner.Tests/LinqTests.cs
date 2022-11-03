65ebc267eacc3bb4455a369674fa893759e414ac
Ben Mazzarol <bmazzarol@gmail.com>
2022-11-03 22:53:49 +0800

    feat: monadic ops on arranged scenarios

---
+++ BunsenBurner.Tests/LinqTests.cs
@@ -0,0 +1,30 @@
+﻿namespace BunsenBurner.Tests;
+
+public static class LinqTests
+{
+    [Fact(DisplayName = "Arranged Aaa scenarios can be selected")]
+    public static async Task Case1() =>
+        await Aaa.Arrange(1).Select(x => x.ToString()).Act(_ => _).Assert(r => r == "1");
+
+    [Fact(DisplayName = "Arranged Aaa scenarios can be selected")]
+    public static async Task Case2() =>
+        await Bdd.Given(1).Select(x => x.ToString()).When(_ => _).Then(r => r == "1");
+
+    [Fact(DisplayName = "Arranged Aaa scenarios can be combined with bind")]
+    public static async Task Case3() =>
+        await Aaa.Arrange(1)
+            .SelectMany(a => Aaa.Arrange(DateTime.Now).Select(b => (a, b)))
+            .Act(_ => _)
+            .Assert(r => r.a == 1 && r.b.Ticks > r.a);
+
+    [Fact(DisplayName = "Arranged Aaa scenarios can be combined with bind")]
+    public static async Task Case4() =>
+        await (
+            from a in Aaa.Arrange(1)
+            from b in Aaa.Arrange(DateTime.Now)
+            from c in Aaa.Arrange(() => "some string")
+            select (a, b, c)
+        )
+            .Act(_ => _)
+            .Assert(r => r.a == 1 && r.b.Ticks > r.a && r.c == "some string");
+}
---
+++ BunsenBurner/Scenario.Linq.cs
@@ -0,0 +1,69 @@
+﻿namespace BunsenBurner;
+
+/// <summary>
+/// Adds linq support to the arrange part of a scenario
+/// </summary>
+public static class Scenario
+{
+    /// <summary>
+    /// Select (Map) for Scenario.Arrange
+    /// </summary>
+    /// <param name="scenario">arranged scenario of TA</param>
+    /// <param name="fn">fn from TA to TB</param>
+    /// <typeparam name="TA">some TA</typeparam>
+    /// <typeparam name="TB">some TB</typeparam>
+    /// <typeparam name="TSyntax">syntax</typeparam>
+    /// <returns>arranged scenario of TB</returns>
+    [Pure]
+    public static Scenario<TSyntax>.Arranged<TB> Select<TSyntax, TA, TB>(
+        this Scenario<TSyntax>.Arranged<TA> scenario,
+        Func<TA, TB> fn
+    ) where TSyntax : struct, Syntax =>
+        new(scenario.Name, async () => fn(await scenario.ArrangeScenario()));
+
+    /// <summary>
+    /// SelectMany (Bind) for Scenario.Arrange
+    /// </summary>
+    /// <param name="scenario">arranged scenario of TA</param>
+    /// <param name="fn">fn from TA to arranged scenario of TB</param>
+    /// <typeparam name="TA">some TA</typeparam>
+    /// <typeparam name="TB">some TB</typeparam>
+    /// <typeparam name="TSyntax">syntax</typeparam>
+    /// <returns>arranged scenario of TB</returns>
+    [Pure]
+    public static Scenario<TSyntax>.Arranged<TB> SelectMany<TSyntax, TA, TB>(
+        this Scenario<TSyntax>.Arranged<TA> scenario,
+        Func<TA, Scenario<TSyntax>.Arranged<TB>> fn
+    ) where TSyntax : struct, Syntax =>
+        new(
+            scenario.Name,
+            async () => await fn(await scenario.ArrangeScenario()).ArrangeScenario()
+        );
+
+    /// <summary>
+    /// SelectMany (Bind) for Scenario.Arrange
+    /// </summary>
+    /// <param name="scenario">arranged scenario of TA</param>
+    /// <param name="mapFn">fn from TA to arranged scenario of TB</param>
+    /// <param name="projectFn">projection function from TA, TB to TC</param>
+    /// <typeparam name="TA">some TA</typeparam>
+    /// <typeparam name="TB">some TB</typeparam>
+    /// <typeparam name="TC">some TC</typeparam>
+    /// <typeparam name="TSyntax">syntax</typeparam>
+    /// <returns>arranged scenario of TC</returns>
+    [Pure]
+    public static Scenario<TSyntax>.Arranged<TC> SelectMany<TSyntax, TA, TB, TC>(
+        this Scenario<TSyntax>.Arranged<TA> scenario,
+        Func<TA, Scenario<TSyntax>.Arranged<TB>> mapFn,
+        Func<TA, TB, TC> projectFn
+    ) where TSyntax : struct, Syntax =>
+        new(
+            scenario.Name,
+            async () =>
+            {
+                var ta = await scenario.ArrangeScenario();
+                var tb = await mapFn(ta).ArrangeScenario();
+                return projectFn(ta, tb);
+            }
+        );
+}
