0fd67c001dbd4398fc864244e22bfcae1cb69b2e
Ben Mazzarol <bmazzarol@gmail.com>
2022-11-19 14:43:17 +0800

    feat(xunit,logging): support for custom logging to output helper

--- BunsenBurner.Logging/DummyLoggerProvider.cs
+++ BunsenBurner.Logging/DummyLoggerProvider.cs
@@ -14,10 +14,12 @@ internal sealed record DummyLoggerProvider : ILoggerProvider
 {
     private readonly LogMessageStore _store;
     private readonly LoggerCache _loggers;
+    private readonly Sink? _sink;
 
-    internal DummyLoggerProvider(LogMessageStore store)
+    internal DummyLoggerProvider(LogMessageStore store, Sink? sink = default)
     {
         _store = store;
+        _sink = sink;
         _loggers = new LoggerCache(StringComparer.Ordinal);
     }
 
@@ -26,7 +28,9 @@ internal sealed record DummyLoggerProvider : ILoggerProvider
             .GetOrAdd(
                 categoryName,
                 ownerClassName =>
-                    new Lazy<DummyLogger<object>>(() => DummyLogger.New(ownerClassName, _store))
+                    new Lazy<DummyLogger<object>>(
+                        () => DummyLogger.New(ownerClassName, _store, _sink)
+                    )
             )
             .Value;
 
---
+++ BunsenBurner.Logging/Sink.cs
@@ -0,0 +1,32 @@
+﻿namespace BunsenBurner.Logging;
+
+/// <summary>
+/// Provides a simple sink for rendered log message strings
+/// </summary>
+public readonly struct Sink
+{
+    private readonly Func<LogMessage, string>? _formatter;
+    private readonly Action<string> _logSink;
+
+    private Sink(Action<string> logSink, Func<LogMessage, string>? formatter)
+    {
+        _logSink = logSink;
+        _formatter = formatter;
+    }
+
+    /// <summary>
+    /// Creates a new sink
+    /// </summary>
+    /// <param name="logSink">log sink action</param>
+    /// <param name="formatter">formatter</param>
+    /// <returns>sink</returns>
+    public static Sink New(Action<string> logSink, Func<LogMessage, string>? formatter = null) =>
+        new(logSink, formatter);
+
+    /// <summary>
+    /// Write a messages to the sink
+    /// </summary>
+    /// <param name="message">message</param>
+    public void Write(LogMessage message) =>
+        _logSink(_formatter?.Invoke(message) ?? message.Message);
+}
